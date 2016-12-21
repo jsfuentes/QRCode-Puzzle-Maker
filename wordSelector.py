6b3564df10574e1e56459769982f94faba199506
LittleSoldierBoy <jsjfuentes>
2016-12-20 16:59:42 -0800

    Created outline for words data structure

---
+++ wordSelector.py
@@ -0,0 +1,11 @@
+f = open('words.txt', 'r')
+words = f.read().split()
+
+#you need a function or something to map every letter to a block number
+dictionary = dict()
+for word in words:
+    pass
+#each word gets all its letters converted to blocknumbers
+#make the block numbers a set or ascending string so every word that fills those blocks has the same key
+#add word to a list with the block numbers filled as the key
+
